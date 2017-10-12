96940496eeb515b758b39f8b5c6429ccede62f1b
mateo-zarrabeitia <mateo.zarrabeitia96@gmail.com>
2017-10-12 12:08:38 -0300

    Actualizando BBDD

--- database/basepizzeria.sql
+++ database/basepizzeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-10-2017 a las 19:45:57
+-- Tiempo de generación: 12-10-2017 a las 17:06:18
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -28,28 +28,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categoria` (
   `id_categoria` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
-(1, 'Pizzas'),
-(5, 'Cosa'),
-(7, 'Empanadas');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `imagen`
---
-
-CREATE TABLE `imagen` (
-  `id_imagen` int(11) NOT NULL,
-  `ruta` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `categoria` (`id_categoria`, `nombre`, `descripcion`) VALUES
+(11, 'Pizzas', 'Pizzas Supremas'),
+(12, 'Ensaladas', 'Para Vegetarianos'),
+(13, 'Empanadas', 'De todos los gustos'),
+(14, 'Pastas', 'Las mas ricas');
 
 -- --------------------------------------------------------
 
@@ -59,18 +50,26 @@ CREATE TABLE `imagen` (
 
 CREATE TABLE `producto` (
   `id_producto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `precio` float NOT NULL
+  `precio` int(11) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `imagenes` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id_producto`, `nombre`, `precio`) VALUES
-(1, 'Muzzarella', 120),
-(5, 'Americana', 150),
-(7, 'Especial', 130);
+INSERT INTO `producto` (`id_producto`, `id_categoria`, `nombre`, `precio`, `descripcion`, `imagenes`) VALUES
+(21, 11, 'Super Suprema', 200, 'Queso mozzarella, pepperoni, jamón, salchicha italiana, champiñones, cebolla, pimiento y aceitunas.', 'images/pizza1.png'),
+(22, 11, 'Chicken BBQ Suprema', 222, 'Queso mozzarella, champiñones, cebolla, fajitas de pollo, tocino y salsa BBQ.', 'images/pizza2.png'),
+(23, 11, 'Meat Suprema', 223, 'Queso mozzarella, pepperoni, jamón, tocino y salchicha italiana.', 'images/pizza3.png'),
+(24, 12, 'Suprema', 100, '¡Nuestra especialidad!. Lechuga, zanahoria, col morada, tomate, jamón, pepperoni, queso mozzarella, ', 'images/acom5.png'),
+(25, 14, 'Lasagna de Pollo o Carne', 130, 'Suave pasta rellana de queso mozzarella y ricotta, cubierta con salla de carne o fajitas de pollo. ', 'images/acom6.png'),
+(26, 13, 'Carne', 10, 'Carne picada', 'images/carne.png'),
+(27, 13, 'Humita', 13, 'Choclo', 'images/humita.png'),
+(30, 13, 'Jamon & Queso', 8, 'Jamon y Queso', 'images/j&q.png');
 
 -- --------------------------------------------------------
 
@@ -90,7 +89,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `password`, `email`) VALUES
-(1, 'mateo', '$2y$10$MU65UxVZS11sMq1FlAmh.OUGvjmEyBi8X9Yue1KphA8.cF3PkGtVC', 'mateo@gmail.com');
+(1, 'mateo', '$2y$10$MU65UxVZS11sMq1FlAmh.OUGvjmEyBi8X9Yue1KphA8.cF3PkGtVC', 'mateo@gmail.com'),
+(2, 'Juan Perez', '$2y$10$cDpsM2GeXpo.BxbNKZqKmuW24ujWFfn7Cr8H/KVA8nh1At3UsgMR6', 'mateo@gmail.com');
 
 --
 -- Índices para tablas volcadas
@@ -103,16 +103,11 @@ ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_categoria`);
 
 --
--- Indices de la tabla `imagen`
---
-ALTER TABLE `imagen`
-  ADD PRIMARY KEY (`id_imagen`);
-
---
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id_producto`);
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_categoria` (`id_categoria`);
 
 --
 -- Indices de la tabla `usuario`
@@ -128,31 +123,26 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT de la tabla `imagen`
---
-ALTER TABLE `imagen`
-  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `categoria`
+-- Filtros para la tabla `producto`
 --
-ALTER TABLE `categoria`
-  ADD CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `producto` (`id_producto`);
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
